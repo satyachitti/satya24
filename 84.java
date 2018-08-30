5c63340acb9e318f1beea757568c0a4f4e4f991f
satyachitti <42774533+satyachitti@users.noreply.github.com>
2018-08-30 05:34:34 +0100

    Create 84.java

---
+++ 84.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+ 
+		Scanner s=new Scanner(System.in);
+		char ch=s.next().charAt(0);
+		System.out.print((int)ch);
+	}
+}
